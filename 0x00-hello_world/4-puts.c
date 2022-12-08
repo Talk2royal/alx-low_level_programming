91351bf60a934eb126d1b50ce2bbb18483a96465
Talk2royal <inaanianiatambari@gmail.com>
2022-12-08 15:32:23 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /*
- * main - code that prints using the put function
+ * main - print using the put function
  * 
  * Return: 0
 */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle"):
-	return (0):
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }		
