bc7dd543e7d01ba6a8777f169de204d641608777
Talk2royal <inaanianiatambari@gmail.com>
2022-12-08 16:36:08 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- * main - print using the put function
- * 
- * Return: 0
-*/
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
-}		
+}
