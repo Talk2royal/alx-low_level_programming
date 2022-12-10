50c24c477c401307b0cb6420b304538b0abf61a5
Talk2royal <inaanianiatambari@gmail.com>
2022-12-10 05:35:59 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,8 +10,9 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
-		putchar(num % 10) + '0');
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+
 	putchar('\n');
 
 	return (0);
