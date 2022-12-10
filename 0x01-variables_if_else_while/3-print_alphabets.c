45571c117de307639437d5ae1b9d5e493b1169da
Talk2royal <inaanianiatambari@gmail.com>
2022-12-10 03:10:35 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,13 @@
 int main(void)
 {
 	char letter;
+	char uppercase;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
-
-	for (letter = 'A'; letter <= 'Z' letter++)
-		putchar(letter);
+	for (uppercase = 'A'; uppercase <= 'Z' uppercase++)
+		putchar(uppercase);
 
 	putchar('\n');
 
