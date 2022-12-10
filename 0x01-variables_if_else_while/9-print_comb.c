b6af6d4a1a0ba53f066b55b70a0ec1d8ff265c25
Talk2royal <inaanianiatambari@gmail.com>
2022-12-10 11:25:31 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,6 +12,7 @@ int main(void)
 	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
+
 		if (num != '9')
 		{
 			putchar(',');
@@ -19,7 +20,7 @@ int main(void)
 		}
 	}
 
-	putchar('\n')
+	putchar('\n');
 
 	return (0);
 }
