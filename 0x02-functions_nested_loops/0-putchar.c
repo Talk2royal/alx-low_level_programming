fc43185db5c02bceed1f4f7603efe798281c3fd0
Talk2royal <inaanianiatambari@gmail.com>
2022-12-13 09:41:32 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Print _putchar
+ *
+ * Return: In this program; 0 (Success)
+ */
+int main(void)
+{
+	char string[8] = "_putchar";
+	int counter = 0;
+
+	while (counter <= 7)
+	{
+		_putchar(string[counter]);
+		counter++;
+	}
+
+	_putchar('\n');
+	return (0);
+}
