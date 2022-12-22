63a33eba5b66ed75d5232ed393b5bb538afdcc6d
Talk2royal <inaanianiatambari@gmail.com>
2022-12-22 07:48:23 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,26 @@
 #include <string.h>
 
 /**
- * *_strcat - concatenate characters
- * @dest: destination character
- * @scr: source character
+ * _strcat - concatenate two strings
+ * @dest: destination input
+ * @scr: source input
  *
  * Return: concatenated string
  */
 
-char *_strcat(char *dest, char *scr)
+char *_strcat(char *dest, char *scr
 {
-	char *result = strcat(dest, src);
+	int c, c2;
 
-	return (result);
+	c = 0;
+	/*find the size of dest array*/
+	while (dest[c])
+		c++;
+
+	/* iterate through each src array value without the null byte*/
+	for (c2 = 0; src[c2] ; c2++)
+		/*append src[c2] to dest[c] while overwritting the null byte in dest*/
+		dest[c++] = src[c2];
+
+	return (dest);
 }
