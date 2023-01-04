89eb2226f3538dc9e77c003b0d75e33c914329c3
Talk2royal <inaanianiatambari@gmail.com>
2023-01-03 21:09:52 -0800

    100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,12 +1,14 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
  * print_diagsums - Print the sum of two diagonals of square matrix
+ *
  * @a: the matrix
  * @size: the size
+ *
  * Return: Nothing
  */
+
 void print_diagsums(int *a, int size)
 {
 	int b, sum1 = 0, sum2 = 0;
