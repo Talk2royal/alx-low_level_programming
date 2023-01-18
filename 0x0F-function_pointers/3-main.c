1b13c89f7bbec3fe61e3cc0c678dba389e689d37
Talk2royal <inaanianiatambari@gmail.com>
2023-01-18 14:37:25 -0800

    3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <string.h> /*for atoi*/
+#include <stdlib.h> /*for exit*/
+
+/**
+ * main - entry point
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: integer value
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*retfunc)(int, int);
+	int retval;
+
+	/* check argument count */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	/* assign values to a and b variables */
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	/* check if operator is a valid operator */
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if (
+			(*(argv[2]) == '/' && b == 0) ||
+			(*(argv[2]) == '%' && b == 0)
+			)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	retfunc = get_op_func(argv[2]);
+	retval = (*retfunc)(a, b);
+
+	printf("%d\n", retval);
+
+	return (0);
+}
