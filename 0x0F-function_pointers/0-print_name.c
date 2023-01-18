99d4a36f39d3f7c2545ee32d08e6c0a19bc040cd
Talk2royal <inaanianiatambari@gmail.com>
2023-01-18 13:24:06 -0800

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+/**
+ * print_name - call a call back function with function pointer supplied
+ * @name: name to be passed as arg to callback
+ * @f: pointer to callback that returns void
+ *
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		(*f)(name);
+	}
+}
